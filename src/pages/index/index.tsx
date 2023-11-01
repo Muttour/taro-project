8e3e1bea4dcee49f7d78a65837e695d6f0ce4aab
qian <2640903995@qq.com>
2023-11-01 14:22:06 +0800

    设置favicon

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import { AtButton, AtAvatar, AtIcon } from 'taro-ui'
+import classNames from 'classnames'
 
-import logoImg from '@/asset/imgs/icon/logo-icon.png'
+import logoIcon from '@/asset/imgs/icon/logo-icon.png'
+import closeIcon from '@/asset/imgs/icon/close-icon.png'
 import CenterContainer from '@/components/CenterContainer'
-import './index.less'
+import cls from './index.module.less'
 
 export default function Index() {
   const [hiddenAd, setHiddenAd] = useState(false)
@@ -13,13 +15,22 @@ export default function Index() {
     console.log('Page loaded.')
   })
 
+  interface Point {
+    x: number
+    y: number
+  }
+
+  let point:Point = { x: 0, y: 0, z: 0 }
+
   return (
     <View className='index'>
       {!hiddenAd && (
-        <View className='at-row '>
+        <View className={classNames('at-row', 'at-row__align--center', cls.commonTip)}>
           <View className='at-col-3'>
-            <AtIcon value='close' size='10' color='#fafafa'></AtIcon>
-            <AtAvatar image={logoImg} size='small'></AtAvatar>
+            <View className='at-row at-row__align--center at-row__justify--around'>
+              <img src={closeIcon} alt='close' className={cls.closeIcon} />
+              <AtAvatar image={logoIcon} size='small' className={cls.logoIcon}></AtAvatar>
+            </View>
           </View>
           <View className='at-col-6'>
             <Text>打开京东APP,实惠又轻松</Text>
